0a3e2763aac6720cbd9f52c14c2a70fd64a15f90
Pedro Ferreira <pedro@bisect.pt>
2025-08-12 16:21:06 +0100

    rust sys: update build.rs to use installed mxl
    If the env var `MXL_INSTALL_ROOT` is defined, use
    `${MXL_INSTALL_ROOT}/include` as the include path.
    
    Signed-off-by: Pedro Ferreira <pedro@bisect.pt>

--- rust/mxl-sys/build.rs
+++ rust/mxl-sys/build.rs
@@ -4,8 +4,10 @@
 use std::env;
 use std::path::PathBuf;
 
+#[cfg(not(feature = "mxl-not-built"))]
 #[cfg(debug_assertions)]
 const BUILD_VARIANT: &str = "Linux-Clang-Debug";
+#[cfg(not(feature = "mxl-not-built"))]
 #[cfg(not(debug_assertions))]
 const BUILD_VARIANT: &str = "Linux-Clang-Release";
 
@@ -14,22 +16,25 @@ struct BindgenSpecs {
     includes_dirs: Vec<String>,
 }
 
-fn get_bindgen_specs() -> BindgenSpecs {
-    #[cfg(not(feature = "mxl-not-built"))]
-    let header = "wrapper-with-version-h.h".to_string();
-    #[cfg(feature = "mxl-not-built")]
-    let header = "wrapper-without-version-h.h".to_string();
+fn get_includes_dirs() -> Vec<String> {
+    if let Ok(install_root) = env::var("MXL_INSTALL_ROOT").map(PathBuf::from) {
+        return vec![install_root.join("include").to_string_lossy().to_string()];
+    }
 
     let manifest_dir =
         PathBuf::from(env::var("CARGO_MANIFEST_DIR").expect("failed to get current directory"));
-    let repo_root = manifest_dir.parent().unwrap().parent().unwrap();
-    let mut includes_dirs = vec![
-        repo_root
-            .join("lib")
-            .join("include")
-            .to_string_lossy()
-            .to_string(),
-    ];
+    let repo_root = manifest_dir.parent().unwrap().parent().unwrap().to_owned();
+    let lib_include_dir = repo_root
+        .join("lib")
+        .join("include")
+        .to_string_lossy()
+        .to_string();
+
+    #[cfg(feature = "mxl-not-built")]
+    {
+        vec![lib_include_dir]
+    }
+
     #[cfg(not(feature = "mxl-not-built"))]
     {
         let build_dir = repo_root.join("build").join(BUILD_VARIANT);
@@ -39,8 +44,17 @@ fn get_bindgen_specs() -> BindgenSpecs {
             .to_string_lossy()
             .to_string();
 
-        includes_dirs.push(build_version_dir);
+        vec![lib_include_dir, build_version_dir]
     }
+}
+
+fn get_bindgen_specs() -> BindgenSpecs {
+    #[cfg(not(feature = "mxl-not-built"))]
+    let header = "wrapper-with-version-h.h".to_string();
+    #[cfg(feature = "mxl-not-built")]
+    let header = "wrapper-without-version-h.h".to_string();
+
+    let includes_dirs = get_includes_dirs();
 
     BindgenSpecs {
         header,
