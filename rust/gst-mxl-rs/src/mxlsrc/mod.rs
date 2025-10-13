ee997f0d2d9567723063440dfcc175238f7a9d31
Simao Fonseca <simao.fonseca@bisect.pt>
2025-10-13 14:38:12 +0000

    gst-plugins: add caps and remove unused code and comments
    Signed-off-by: Simao Fonseca <simao.fonseca@bisect.pt>

--- rust/gst-mxl-rs/src/mxlsrc/mod.rs
+++ rust/gst-mxl-rs/src/mxlsrc/mod.rs
@@ -13,19 +13,15 @@ use gst::prelude::*;
 
 mod imp;
 
-// The public Rust wrapper type for our element
 glib::wrapper! {
-    pub struct SineSrc(ObjectSubclass<imp::SineSrc>) @extends gst_base::PushSrc, gst_base::BaseSrc, gst::Element, gst::Object;
+    pub struct MxlSrc(ObjectSubclass<imp::MxlSrc>) @extends gst_base::PushSrc, gst_base::BaseSrc, gst::Element, gst::Object;
 }
 
-// Registers the type for our element, and then registers in GStreamer under
-// the name "sinesrc" for being able to instantiate it via e.g.
-// gst::ElementFactory::make().
 pub fn register(plugin: &gst::Plugin) -> Result<(), glib::BoolError> {
     gst::Element::register(
         Some(plugin),
-        "rssinesrc",
+        "rsmxlsrc",
         gst::Rank::NONE,
-        SineSrc::static_type(),
+        MxlSrc::static_type(),
     )
 }
